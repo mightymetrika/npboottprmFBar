aeb98eb8ec3ffd2f3cd211d6480f6057007eff71
Mackson Ncube <macksonncube@gmail.com>
2023-12-09 17:28:48 +0000

    work on bootFbar and persimon_TypeI

--- tests/testthat/test-bootFbar.R
+++ tests/testthat/test-bootFbar.R
@@ -5,6 +5,9 @@
 #   pval_B <- iht_res$B$pvalue[1]
 #   pval_A <- iht_res$A$pvalue[1]
 #
+#
+#
+#
 #   FbarOut <- bootFbar(data = iris, formula = "Sepal.Length ~ -1 + Species", grp = "Species",
 #            constraints = 'Speciessetosa < Speciesversicolor < Speciesvirginica',
 #            nboot = 100, conf.level = 0.95)
@@ -21,5 +24,16 @@
 #                       constraints = 'Speciesversicolor < Speciessetosa <  Speciesvirginica',
 #                       nboot = 100, conf.level = 0.95)
 #
+#
+#
+#   FbarOut <- bootFbar(data = iris, formula = "Sepal.Length ~ -1 + Species + Sepal.Width", grp = "Species",
+#                       constraints = 'Speciesversicolor < Speciessetosa <  Speciesvirginica',
+#                       nboot = 100, conf.level = 0.95)
+#
+#   iris2 <- iris
+#   iris2$Sepal.Width <- 0
+#   FbarOut <- bootFbar(data = iris2, formula = "Sepal.Length ~ -1 + Species + Sepal.Width", grp = "Species",
+#                       constraints = 'Speciesversicolor < Speciessetosa <  Speciesvirginica',
+#                       nboot = 100, conf.level = 0.95)
 #   expect_equal(2 * 2, 4)
 # })
